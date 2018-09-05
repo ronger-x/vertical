80c85dd29b56407aa4974d3d5fb91d9a92dfbbad
ronger <ronger-x@outlook.com>
2018-09-05 17:34:24 +0800

    背景图片添加

--- src/test/java/com/voally/vertical/VerticalApplicationTests.java
+++ src/test/java/com/voally/vertical/VerticalApplicationTests.java
@@ -1,12 +1,9 @@
 package com.voally.vertical;
 
-import com.voally.vertical.util.GlobalFunc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.text.ParseException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,11 +11,6 @@ public class VerticalApplicationTests {
 
     @Test
     public void contextLoads() {
-        try {
-            GlobalFunc.getTimeAgo(1535420978321L);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
     }
 
 }
