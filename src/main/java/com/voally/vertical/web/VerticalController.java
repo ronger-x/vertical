a5d10356b97024c3a151269e778a421069c78795
ronger <ronger-x@outlook.com>
2018-09-19 17:38:37 +0800

    首页流加载实现

--- src/main/java/com/voally/vertical/web/VerticalController.java
+++ src/main/java/com/voally/vertical/web/VerticalController.java
@@ -1,29 +1,59 @@
 package com.voally.vertical.web;
 
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.voally.vertical.api.entity.ArticleDTO;
 import com.voally.vertical.api.entity.Thinker;
+import com.voally.vertical.core.result.GlobalResult;
+import com.voally.vertical.core.result.GlobalResultGenerator;
+import com.voally.vertical.core.result.Pagination;
 import com.voally.vertical.util.ArticleUtils;
 import com.voally.vertical.util.VerticalUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class VerticalController {
 
     @GetMapping(value = {"/","/index"})
-    public String index(HttpServletRequest request, Model model){
+    public String index(HttpServletRequest request, Model model) throws IOException {
         List<ArticleDTO> list = VerticalUtils.getArticles();
         model.addAttribute("list",list);
         List<Thinker> thinkers = VerticalUtils.getRecommendThinkers();
         model.addAttribute("thinkers",thinkers);
         List<ArticleDTO> articles = VerticalUtils.getRecommendArticles();
         model.addAttribute("articles",articles);
+
+        VerticalUtils.getUserAgent();
+
         return "index";
+    }
+
+    @RequestMapping(value = "/getArticles", method = RequestMethod.GET)
+    @ResponseBody
+    public GlobalResult getArticles(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "20") Integer size, HttpServletRequest request){
+        PageHelper.startPage(page, size);
+        List<ArticleDTO> list = VerticalUtils.getArticles();
+        PageInfo pageInfo = new PageInfo(list);
+        Map result = new HashMap();
+        result.put("data",pageInfo.getList());
+        Pagination pagination = new Pagination();
+        pagination.setPaginationPage(pageInfo.getPageNum());
+        pagination.setPaginationSize(pageInfo.getPageSize());
+        pagination.setPaginationPageCount(pageInfo.getTotal());
+        pagination.setNextPage(pageInfo.getNextPage());
+        pagination.setPrePage(pageInfo.getPrePage());
+        pagination.setPaginationPageNums(VerticalUtils.getPaginationPageNums(pagination.getPaginationPageCount(),pagination.getPaginationSize()));
+        result.put("pagination",pagination);
+        return GlobalResultGenerator.genSuccessResult(result);
     }
 
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,48 +1,64 @@
-var voally = {
-    pagination: function(data){
-        var page = data.paginationPage;
-        var rows = data.paginationRows;
-        var count = data.paginationPageCount;
-        var nums = data.paginationPageNums;
-        var paginationHtml = "";
-        if(page>1){
-            paginationHtml += "<li class=\"page-item page-prev\">\n" +
-                "                                <a class=\"page-link\" href=\"javascript:prev("+page+");\" tabindex=\"-1\">\n" +
-                "                                    上一页\n" +
-                "                                </a>\n" +
-                "                            </li>";
-        }else {
-            paginationHtml += "<li class=\"page-item page-prev disabled\">\n" +
-                "                                <a class=\"page-link\" href=\"javascript:prev("+page+");\" tabindex=\"-1\">\n" +
-                "                                    上一页\n" +
-                "                                </a>\n" +
-                "                            </li>";
-        }
-        for(var i=1,len=nums.length;i<=len;i++){
-            if(page==i){
-                paginationHtml += "<li class=\"page-item active\"><a class=\"page-link\" href=\"javascript:go("+page+")\">"+i+"</a></li>";
-            }else {
-                paginationHtml += "<li class=\"page-item\"><a class=\"page-link\" href=\"javascript:go("+page+")\">"+i+"</a></li>";
-            }
-        }
+Vue.component('articles-component',{
+    props: ['article'],
+    template:'<div class="col-lg-12">\n' +
+    '                                <div class="card">\n' +
+    '                                    <div class="card-body d-flex flex-column">\n' +
+    '                                        <h4><a v-bind:href="article.articlePermalink">{{ article.articleTitle }}</a></h4>\n' +
+    '                                        <div class="text-muted article-summary-md" >{{ article.articleSummary }} \n' +
+    '                                        </div>\n' +
+    '                                        <div class="d-flex align-items-center pt-5 mt-auto">\n' +
+    '                                            <div class="avatar avatar-md mr-3"\n' +
+    '                                                 v-bind:style="\'background-image: url(\'+ article.thinker.userAvatarUrl +\')\'">\n' +
+    '                                            </div>\n' +
+    '                                            <div>\n' +
+    '                                                <a v-bind:href="article.thinker.userUrl" class="text-default">{{ article.thinker.nickName }}</a>\n' +
+    '                                                <small class="d-block text-muted" >{{ article.timeAgo }}</small>\n' +
+    '                                            </div>\n' +
+    '                                            <div class="ml-auto text-muted">\n' +
+    '                                                <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i\n' +
+    '                                                        class="fe fe-heart mr-1"></i></a>\n' +
+    '                                            </div>\n' +
+    '                                        </div>\n' +
+    '                                    </div>\n' +
+    '                                </div>\n' +
+    '                            </div>'
+});
 
-        if(page<nums.length){
-            paginationHtml += "<li class=\"page-item page-prev\">\n" +
-                "                                <a class=\"page-link\" href=\"javascript:next("+page+");\" tabindex=\"-1\">\n" +
-                "                                    下一页\n" +
-                "                                </a>\n" +
-                "                            </li>";
-        }else {
-            paginationHtml += "<li class=\"page-item page-prev disabled\">\n" +
-                "                                <a class=\"page-link\" href=\"javascript:next("+page+");\" tabindex=\"-1\">\n" +
-                "                                    下一页\n" +
-                "                                </a>\n" +
-                "                            </li>";
+var vm = new Vue({
+    el:'#articles-content',
+    data:{
+        articles:[],
+        pagination:{
+            paginationPage: '1',
+            paginationSize: '20'
         }
-
-        $(".pagination").prop("data-page",page);
-
-        $(".pagination").html("");
-        $(".pagination").append(paginationHtml);
     }
+});
+
+function getArticles(page,size){
+    $.get("/getArticles",{page: page, size: size},function (data) {
+        var articles = data.data.data;
+        var pagination = data.data.pagination;
+        for (var i in articles){
+            vm.articles.push(articles[i]);
+        }
+        // 更新分页信息
+        vm.pagination = pagination;
+    })
 }
+
+$(function () {
+    getArticles(vm.pagination.paginationPage,vm.pagination.paginationSize);
+});
+
+//jquery 监听页面滚动至页面底部
+$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    var scrollHeight = $(document).height();
+    var windowHeight = $(this).height();
+    if(scrollTop + windowHeight == scrollHeight){
+        if(vm.pagination.nextPage && vm.pagination.nextPage >0){
+            getArticles(vm.pagination.nextPage,vm.pagination.paginationSize);
+        }
+    }
+});
